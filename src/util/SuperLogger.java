3560efc70058e96553277a85cba665c8fc3790ac
Priscila <priscilaibarra2015@hotmail.com>
2017-06-26 05:31:44 -0300

    metodo registrar cliente completado faltan agregar validaciones en el formulario

--- src/util/SuperLogger.java
+++ src/util/SuperLogger.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 
-public class SuperLogger {
+public class SuperLogger { 
 	public static final Logger logger = 
 			LogManager.getLogger(SuperLogger.class.getName());
 
